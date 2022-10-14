fd3fbe70bf4896fdbd9bf447bf95ee203dd03867
AngeloAbat <angeloabat25@gmail.com>
2022-10-13 20:01:37 -0700

    Re-filled Everything

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import React from "react";
+import './App.css';
+import Home from './components/home';
+import Test from './components/test'
+import Bar from './components/navbar.js';
+
+
+function App() {
+
+  return (
+    
+    <Router>
+    <div className="App">
+      <div className="display">
+        <Bar/>
+        <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/test" element={<Test />} />
+            </Routes>
+        </main>
+      </div>
+    </div>
+    </Router>
+  );
+}
+export default App;
