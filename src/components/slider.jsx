82cdf79bd14caa4f827588d715e537a9fc425380
johnathanwastaken <danielortiz2001@hotmail.com>
2022-10-15 17:05:05 -0700

    Added a data.js to be able to see if the slider changes info, trying to fix a not defined variable for the coding to work

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -1,12 +1,14 @@
 import { KeyboardArrowLeftOutlined, KeyboardArrowRightOutlined } from "@mui/icons-material"
 import styled from "styled-components"
 import SlideImg from "../img/slideImg.png"
+import { sliderItems } from "../data";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -27,10 +29,14 @@ const Arrow = styled.div`
   margin: auto;
   cursor: pointer;
   opacity: 0.5;
+  z-index: 2;
 `;
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: tramslateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
@@ -38,6 +44,7 @@ const Slide = styled.div`
   height 100vh;
   display: flex;
   align-items: center;
+  background-color: #${props=>props.bg};
 `;
 
 const ImgContainer = styled.div`
@@ -57,28 +64,49 @@ const InfoContainer = styled.div`
 const Title = styled.h1`
   font-size: 70px;
 `
-const Desc = styled.p``
-const Button = styled.button``
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px
+`
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+    const handleClick = (direction) => {
+      if(direction==="left"){
+        setSlideIndex(slideIndex > 0 ? slideIndex-1 : 1);
+      } else{
+        setSlideIndex(slideIndex < 1 ? slideIndex +1 : 0);
+      }
+    };
+
   return (
     <Container>
-        <Arrow direction='left'>
+        <Arrow direction='left' onClick={()=>handleClick("left")}>
             <KeyboardArrowLeftOutlined/>
         </Arrow>
-        <Wrapper>
-              <Slide>
+        <Wrapper slideIndex={slideIndex}>
+          {sliderItems.map(item=>(
+              <Slide bg={item.bg}>//This is to change the color once we figure that out all the information can be changed from data.js"
             <ImgContainer>
-              <Image src='https://pngtree.com/freepng/scary-halloween_5946675.html'/>
+              <Image src={item.img}/>
             </ImgContainer>
             <InfoContainer>
-              <Title>Halloween Sale</Title>
-              <Desc>Don't wait now Halloween is coming! Get 30% off with any purchase above $50!</Desc>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
               <Button>Shop Now</Button>            
             </InfoContainer>
               </Slide>
+          ))}    
         </Wrapper>
-        <Arrow direction="right">
+        <Arrow direction="right" onClick={()=>handleClick("right")}>
             <KeyboardArrowRightOutlined/>
         </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "",
+      title: "Halloween SALE",
+      desc: "Don't wait now Halloween is coming! Get 30% off with any purchase above $50!",
+      bg: "",
+    },
+    {
+      id: 2,
+      img: "",
+      title: "Costume Collection",
+      desc: "Don't compromise on style! Get flat 30% off for new arrivals with any purchase above $50!",
+      bg: "",
+    },
+  ];
