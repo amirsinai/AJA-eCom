83db5141494f01abed7399fef697c766be7e3305
johnathanwastaken <danielortiz2001@hotmail.com>
2022-10-16 13:24:06 -0700

    Updated Contact Information

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -128,13 +128,13 @@ const Footer = () => {
       <Right>
         <Title>Contact</Title>
         <ContactItem>
-          <Room style={{marginRight:"10px"}}/> 622 Dixie Path , South Tobinchester 98336
+          <Room style={{marginRight:"10px"}}/> 647 Winchester Court Ellenwood, GA 30294
         </ContactItem>
         <ContactItem>
-          <Phone style={{marginRight:"10px"}}/> +1 234 56 78
+          <Phone style={{marginRight:"10px"}}/> 1-888-447-5594
         </ContactItem>
         <ContactItem>
-          <MailOutline style={{marginRight:"10px"}} /> contact@lama.dev
+          <MailOutline style={{marginRight:"10px"}} /> Jasonwashere@gmail.com
         </ContactItem>
         <Payment src='https://i.ibb.co/Qfvn4z6/payment.png' />
       </Right>
