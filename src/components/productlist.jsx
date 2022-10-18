85a9f213343bec6cb2edcc2bd4e7089b56bb10ea
johnathanwastaken <danielortiz2001@hotmail.com>
2022-10-18 13:00:31 -0700

    Added the coding to get the information from the api and made a seperate file to make the styling hopefully this works

---
+++ src/components/productlist.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+import { useState, useEffect } from "react"
+import Product from "./productstyling";
+
+const Container = styled.div`
+    padding: 20px;
+    display: flex;
+`
+
+const ProductList = () => {
+    const[testData, setTestData] = useState([])
+
+    useEffect(() => {
+        const SearchEndPoint = `https://qndk0sl6mi.execute-api.us-west-2.amazonaws.com/items`
+        const fetchData = async() => {
+            const response = await fetch(SearchEndPoint)
+            const resData = await response.json()
+            setTestData(resData.Items)
+        }
+        fetchData()
+    }, [])
+
+    console.log(testData)
+
+        return(
+            <Container>
+                {testData.map((item, index) => (
+                    <Product item={item.ItemName} key={index} />
+                ))}
+            </Container>
+        )
+    }
+
+export default ProductList
---
+++ src/components/productstyling.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+
+`
+
+const productstyling = ({item, index}) => {
+  return (
+    <Container>
+        <Circle/>
+        <Image src={}
+    </Container>
+  )
+}
+
+export default productstyling
