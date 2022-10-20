36f767ece478a2d841b9f0250cd5dc2c1aef2c0f
johnathanwastaken <danielortiz2001@hotmail.com>
2022-10-19 17:46:19 -0700

    Updated the icons and now the icons are centered, also made a hover for the icons, will try to make the cart by the end of the day. Might be difficult to finish by tommrow.

--- src/components/productstyling.jsx
+++ src/components/productstyling.jsx
@@ -1,31 +1,54 @@
 import styled from "styled-components"
 import { SearchOutlined, ShoppingCartOutlined, FavoriteBorderOutlined } from "@mui/icons-material";
 
+const Info = styled.div`
+opacity: 0;
+width: 100%;
+height: 100%;
+position: absolute;
+top: 0;
+left: 0;
+z-index: 3;
+display: flex;
+align-items: center;
+justify-content: center;
+transition: all 0.5s ease;
+cursor: pointer;
+`;
+
 const Container = styled.div`
-    flex: 1;
-    margin: 5px;
-    min-width: 280px;
-    height: 350px;
-    display: flex;
-    align-item: center;
-    justify-content: center;
-    background-color: 
-    position: relative;
+flex: 1;
+margin: 5px;
+min-width: 280px;
+height: 350px;
+display: flex;
+align-items: center;
+justify-content: center;
+position: relative;
+&:hover ${Info}{
+  opacity: 1;
+}
 `;
 
 const Image = styled.img`
     height: 75%
 `;
 
-const Info = styled.div`
-    width: 100%;
-    height: 100%;
-    align-item: center;
-    position: absolute;
-
-`;
 const Icon = styled.div`
-    color: red;
+    color: black;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+    transition: all 0.5s ease;
+    &:hover {
+        background-color: #e9f5f5;
+        transform: scale(1.1);
+  }
 `;
 
 const productstyling = ({item, index}) => {
@@ -34,7 +57,7 @@ const productstyling = ({item, index}) => {
     <Container>
         <Image src={item.Image}/>
         <Info>
-            <Icon>
+            <Icon container direction="row">
                 <ShoppingCartOutlined/>
             </Icon>
             <Icon>
