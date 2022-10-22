f403bc240ab028976a63aab1e54197b4b4ba8158
johnathanwastaken <danielortiz2001@hotmail.com>
2022-10-22 13:04:05 -0700

    adding styling to individual items

--- src/components/productItemPagestyling.jsx
+++ src/components/productItemPagestyling.jsx
@@ -1,11 +1,19 @@
 import styled from "styled-components"
 import { useState, useEffect} from "react"
 import { useParams } from "react-router-dom"
+import Newsletter from "./newsletter.jsx"
+import { Add, Remove } from "@mui/icons-material"
 
-const Container = styled.div``;
+const Container = styled.div`
+    
+`;
 const Wrapper = styled.section`
     padding: 50px;
     display: flex;
+    justify-content: center;
+    align-item: center;
+    position: relative;
+    height 113vh;
 `;
 
 const ImgContainer = styled.div`
@@ -16,31 +24,70 @@ const Image = styled.img`
     width: 100%;
     height: 90vh;
     object-fit: cover;
+    border-radius: 25px;
+    padding: 20px;
 `;
 
 const InfoContainer = styled.div`
     flex: 1;
-    padding: 0px 50px;
+    padding: 50px 40px;
 `;
 
 const Title = styled.div`
-    font-weight: 200;
+    font-weight: bold;
+    font-size: 60px;
+    background-image: linear-gradient(45deg, #f3ec78, #af4261);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
 `;
 
 const Desc = styled.div`
     margin: 20px 0px;
+    font-weight: 500;
+    font-size: 24px
 `;
 
 const Price = styled.div`
     font-weight: 100;
     font-size: 40px;
 `;
-const AddContainer = styled.div``;
-const AmountContainer = styled.div``;
-const Remove = styled.div``;
-const Amount = styled.div``;
-const Add = styled.div``;
-const Button = styled.button``;
+
+const AddContainer = styled.div`
+    width: 50%
+    display: flex;
+    align-item: center;
+    justify-content: space-between;
+    `;
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+    cursor: pointer;
+`;
+
+const Amount = styled.div`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`;
+
+const Button = styled.button`
+    padding: 15px;
+    border: 2px solid teal;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 500;
+    &:hover{
+        background-color: #f8f4f4;
+    }
+`;
 
 const ProductItemPagestyling = () => {
     const { id } = useParams()
@@ -56,6 +103,16 @@ const ProductItemPagestyling = () => {
         fetchData()
     }, [])
 
+    const [quantity, setQuantity] = useState(1);
+
+    const handleQuantity = (type) =>{
+        if(type === "dec") {
+            quantity > 1 && setQuantity(quantity - 1); 
+        } else {
+            setQuantity(quantity + 1);
+        }
+    };
+
   return (
     <div>
         <Container>
@@ -69,14 +126,15 @@ const ProductItemPagestyling = () => {
                     <Price>${productData.Price}</Price>
                     <AddContainer>
                         <AmountContainer>
-                            <Remove/>
-                            <Amount>1</Amount>
-                            <Add />
+                            <Remove onClick={()=>handleQuantity("dec")}/>
+                            <Amount>{quantity}</Amount>
+                            <Add onClick={()=>handleQuantity("inc")}/>
                         </AmountContainer>
                         <Button>ADD TO WISHLIST</Button>
                     </AddContainer>
                 </InfoContainer>
             </Wrapper>
+            <Newsletter />
         </Container>
     </div>
   )
